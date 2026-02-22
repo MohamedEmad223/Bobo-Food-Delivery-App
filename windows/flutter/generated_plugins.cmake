92fc3edba9636b03fdaeed12c0061d191a171e09
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2026-02-23 01:31:54 +0200

    finish Add core folders and init the App

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,9 +3,18 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  audioplayers_windows
+  cloud_firestore
+  firebase_auth
+  firebase_core
+  flutter_secure_storage_windows
+  local_auth_windows
+  secure_application
+  sentry_flutter
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
