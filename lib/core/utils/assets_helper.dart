746632f678b2e6efea7840e255334a17ac8e8696
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2026-03-06 00:46:11 +0200

    - add assets to assets helper

---
+++ lib/core/utils/assets_helper.dart
@@ -0,0 +1,9 @@
+class AssetsHelper{
+  AssetsHelper._();
+static const String backGroundSplashScreenImage = 'assets/images/splash/Splash_light_background.png';
+static const String backGroundSplashScreenImageDark = 'assets/images/splash/Splash_dark_background.png';
+static const String logoSplashScreenImage = 'assets/images/splash/logo.png';
+static const String backGroundPatternImageSplashScreenImage = 'assets/images/splash/pattern.png';
+static const String backGroundPatternImageSplashScreenImageDark = 'assets/images/splash/pattern_dark.png';
+
+}
