045b4794869eab16b630237f08144c05bcca40d9
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2026-03-12 14:35:40 +0200

    add pin put widget

--- lib/core/theme/app_text_styles.dart
+++ lib/core/theme/app_text_styles.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'color_helper.dart';
 
@@ -131,6 +132,15 @@ class AppTextStyles {
       color: isDark
           ? ColorHelper.textPrimaryDark
           : ColorHelper.textPrimaryLight,
+    );
+  }
+
+  static TextStyle heading3(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    return GoogleFonts.poppins(
+      fontSize: 20.0.sp,
+      fontWeight: FontWeight.w500,
+      color: isDark ? ColorHelper.textPrimaryDark : ColorHelper.grey,
     );
   }
 
